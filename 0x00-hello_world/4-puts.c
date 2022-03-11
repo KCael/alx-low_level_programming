543447ca1a16847954c5e61557974067bc0409ff
KCael <katenganga22@gmail.com>
2022-03-11 02:12:09 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the puts statement followed by a new line
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
