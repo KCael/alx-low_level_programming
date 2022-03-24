6e20681446968f82999d213796da8a710079b04d
KCael <katenganga22@gmail.com>
2022-03-24 10:47:42 -0700

    concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - appends one string onto the other
+ *
+ *@dest: the first string
+ *@src: the second string
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	char *dest = "Hello";
+	char *src = "World!";
+
+	/*appends dest onto src*/
+	strcat(dest, src);
+	printf("strcat(dest, src): %s\n", dest);
+
+	return (0);
+}
