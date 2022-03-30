bcda3c8e10eb2c50a0926621a88042a9daf22215
KCael <katenganga22@gmail.com>
2022-03-30 12:04:55 -0700

    Prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,6 @@
 /**
  * _puts_recursion - prints a string
  * @s: holds the string
- * return: a string
  */
 void _puts_recursion(char *s)
 {
@@ -13,5 +12,5 @@ void _puts_recursion(char *s)
 		return;
 	}
 	_putchar(*s);
-	_puts_recursion(*s + 1);
+	_puts_recursion(s + 1);
 }
