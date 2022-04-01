60c34f2d21b60d12e7c66086bb623da869796365
KCael <katenganga22@gmail.com>
2022-04-01 15:49:10 -0700

    creates a static library called liball.a from all the .c files that are in the current directory.

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenates two strings
+ *
+ *@dest: the first string
+ *@src: the second string
+ *@n: number of bytes
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
